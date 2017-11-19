9a09bd745c905fa6ba711f5db4b639c1967d70bf
Mina Sameh <minasamehlabib@gmail.com>
2017-11-20 01:33:44 +0200

    replaced password with code

--- app/src/main/java/com/mina/collegehelper/activities/Login.java
+++ app/src/main/java/com/mina/collegehelper/activities/Login.java
@@ -22,16 +22,16 @@ public class Login extends BaseActivity {
 
     private String EMAIL_NOT_VERIFIED = "Please verify your email first";
     private String INVALID_EMAIL = "Invalid or empty Email";
-    private String INVALID_PASSWORD = "Invalid or empty password";
+    private String INVALID_CODE = "Invalid or empty Code";
 
     @BindView(R.id.emailTextView) TextView emailTextView;
-    @BindView(R.id.passwordTextView) TextView passwordTextView;
+    @BindView(R.id.codeTextView) TextView codeTextView;
     @BindView(R.id.loginButton) Button loginButton;
     @BindView(R.id.regButton) Button regButton;
 
     private ProgressDialog dialog;
     private String email;
-    private String password;
+    private String code;
 
 
     @Override
@@ -57,7 +57,7 @@ public class Login extends BaseActivity {
 
     private void collectParameters() {
         email = emailTextView.getText().toString();
-        password = passwordTextView.getText().toString();
+        code = codeTextView.getText().toString();
     }
 
     private boolean validateParameters() {
@@ -67,16 +67,16 @@ public class Login extends BaseActivity {
             return false;
         }
 
-        if (!Validator.validatePassword(password)) {
-            passwordTextView.requestFocus();
-            passwordTextView.setError(INVALID_PASSWORD);
+        if (!Validator.validateCode(code)) {
+            codeTextView.requestFocus();
+            codeTextView.setError(INVALID_CODE);
             return false;
         }
         return true;
     }
 
     private void login() {
-        AuthenticationHelper.login(email, password, new ServerCallback() {
+        AuthenticationHelper.login(email, code, new ServerCallback() {
             @Override
             public void onFinish(ServerResponse response) {
                 dialog.hide();
